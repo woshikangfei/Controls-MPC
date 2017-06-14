61b5fc9616f892d92cc5479a2e5209838920b9af
kangfei <kang_chao520@163.com>
2017-06-14 17:07:00 +0800

    Create MPC.h
    modify https://github.com/tho15/carnd-mpc-control-project/blob/master/src/MPC.h

--- src/MPC.h
+++ src/MPC.h
@@ -12,6 +12,9 @@ class MPC {
 
   virtual ~MPC();
 
+  vector<double> solx_;
+  vector<double> soly_;
+  double curSteering_;  
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuatotions.
   vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
